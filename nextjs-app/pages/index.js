990e91ae8229218076fa8028cacf9d87b8860460
Chouaib <chouaib.skt@gmail.com>
2024-04-28 20:11:38 +0200

    update

--- nextjs-app/pages/index.js
+++ nextjs-app/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Hola !</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -13,6 +13,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <p className={styles.description}>CS</p>
 
         <p className={styles.description}>
           Get started by editing <code>pages/index.js</code>
